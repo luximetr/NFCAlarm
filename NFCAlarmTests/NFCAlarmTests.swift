e10e68f45c423487b58e688eb383ff67e7088bbe
luximetr <luximetr.sync@gmail.com>
2025-08-26 22:09:26 +0800

    Initiated project

---
+++ NFCAlarmTests/NFCAlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFCAlarmTests.swift
+//  NFCAlarmTests
+//
+//  Created by Oleksandr Orlov on 26/8/25.
+//
+
+import Testing
+@testable import NFCAlarm
+
+struct NFCAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

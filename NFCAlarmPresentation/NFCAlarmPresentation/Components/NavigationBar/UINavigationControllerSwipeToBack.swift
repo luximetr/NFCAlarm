8e1a2d36232022d8b4b5e181d3d9fd058cfaa0f7
luximetr <luximetr.sync@gmail.com>
2025-09-12 20:36:30 +0800

    Fixed swipe to back when hide navigation bar

---
+++ NFCAlarmPresentation/NFCAlarmPresentation/Components/NavigationBar/UINavigationControllerSwipeToBack.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.isEnabled = true
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        if interactivePopGestureRecognizer == gestureRecognizer {
+            return viewControllers.count > 1
+        }
+        return true
+    }
+}
